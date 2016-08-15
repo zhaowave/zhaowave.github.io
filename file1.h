8e747fee57fef163720b0e12238a68483fdbab62
will zhao <674524145@qq.com>
2016-08-15 22:34:52 +0800

    add file1.h and modify index.html

---
+++ file1.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: file1.h
+    > Author: Mr.zhao
+    > Mail: zhao_wei@bupt.edu.cn 
+    > Created Time: 2016年08月15日 星期一 22时06分47秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+typedef struct {
+	int a;
+	int b;
+}hello;
+
+in test branch
