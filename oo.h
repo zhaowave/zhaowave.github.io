322f49a03ef17a738cf102aafbfcb7be70f573c5
will zhao <674524145@qq.com>
2016-08-17 23:31:23 +0800

    oo.h

---
+++ oo.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: oo.h
+    > Author: Mr.zhao
+    > Mail: zhao_wei@bupt.edu.cn 
+    > Created Time: 2016年08月17日 星期三 23时30分59秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+lfdjfdkla
